458e09a81f9f75dce04dd3df0cdc3a055069126f
Juan <joanjerezt@gmail.com>
2017-05-25 01:21:34 +0200

    Versión BETA

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,11 +2,14 @@
 #include "ui_mainwindow.h"
 #include "about.h"
 
+QString fileName;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->actionQuant_a,SIGNAL(triggered()),this,SLOT(obrir_about()));
 }
 
 MainWindow::~MainWindow()
@@ -20,11 +23,9 @@ void MainWindow::on_pushButton_clicked()
     double newCO2 = ui->doubleSpinBox_CO2->value();
     double newNO2 = ui->doubleSpinBox_NO2->value();
     QString newLocation = ui->comboBox_2->currentText();
-    QString newJARPath;
 
-
-    //QString resultat = sendData(newDay,newCO2,newNO2,newLocation,newJARPath);
-    QString resultat = "Èxit";
+    QString resultat = sendData(newDay,newCO2,newNO2,newLocation);
+    //QString resultat = "Èxit";
     ui->textBrowser->insertPlainText(resultat);
 }
 
@@ -36,12 +37,35 @@ void MainWindow::obrir_about()
     delete a;
 }
 
-QString MainWindow::sendData (QString day,double co2, double no2,QString location, QString jarpath) {
+QString MainWindow::sendData (QString day,double co2, double no2,QString location) {
 
-   QString resposta = day + "/" + co2 + "/" + no2 + "/" + location + "/" + jarpath;
+    QLocale::setDefault(QLocale(QLocale::Catalan));
+    QLocale locale; // Constructs a default QLocale
+
+    QString CO2_text = locale.toString(co2, 'f', 2);
+    QString NO2_text = locale.toString(no2, 'f', 2);
+
+   QString execute;
+
+   execute="java -jar " + fileName + " -d " + day + " -c " + CO2_text + " -n " + NO2_text + " -l " + location;
+
+   weka.start(execute);
+   weka.waitForFinished();
+   QString resposta = weka.readAllStandardOutput();
+
+
+   //QString resposta = day + " / " + CO2_text + " / " + NO2_text + " / " + location + " / " + fileName;
 
    return resposta;
 
 }
 
+void MainWindow::on_toolButton_clicked()
+{
 
+    fileName = QFileDialog::getOpenFileName(this,
+            tr("Selecciona l'aplicació"), "",
+            tr("Java Archive (*.jar);;Tots els fitxers (*)"));
+    ui->executableTextEdit->setPlainText(fileName);
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QProcess>
+#include <QFileDialog>
 
 namespace Ui {
 class MainWindow;
@@ -18,10 +20,13 @@ public:
 private slots:
     void on_pushButton_clicked();
     void obrir_about();
-    QString sendData (QString day, double co2, double no2, QString location, QString JARpath);
+    QString sendData (QString day, double co2, double no2, QString location);
+
+    void on_toolButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    QProcess weka;
 };
 
 #endif // MAINWINDOW_H
